45071948696249c017cba91ec4cc3200894af017
Debanjan Sengupta <dave4u90@gmail.com>
2020-02-23 13:51:14 +0530

    Included all the currencies supported

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,2 +1,21 @@
-Currency.where(name: 'Bitcoin', code: 'BTCAUD').first_or_create
-Currency.where(name: 'Ethereum', code: 'ETHAUD').first_or_create
+# Source https://api.exchange.coinjar.com/products
+
+[
+  { code: "BTCAUD", name: "BTC/AUD" },
+  { code: "ETHAUD", name: "ETH/AUD" },
+  { code: "ETHBTC", name: "ETH/BTC" },
+  { code: "XRPAUD", name: "XRP/AUD" },
+  { code: "XRPBTC", name: "XRP/BTC" },
+  { code: "LTCAUD", name: "LTC/AUD" },
+  { code: "LTCBTC", name: "LTC/BTC" },
+  { code: "ZECAUD", name: "ZEC/AUD" },
+  { code: "ZECBTC", name: "ZEC/BTC" },
+  { code: "BTC-USDC", name: "BTC/USDC" },
+  { code: "USDC-AUD", name: "USDC/AUD" },
+  { code: "ZRXBTC", name: "ZRX/BTC" },
+  { code: "ZRXAUD", name: "ZRX/AUD" },
+  { code: "BATBTC", name: "BAT/BTC" },
+  { code: "BATAUD", name: "BAT/AUD" }
+].each do |product|
+  Currency.where(product).first_or_create
+end
